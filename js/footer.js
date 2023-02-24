ab98a37ea52306490ce6479bf9c0bd2a45291a13
Him97 <78652542+Him97@users.noreply.github.com>
2023-02-24 23:31:18 +0200

    V0.7

--- js/footer.js
+++ js/footer.js
@@ -7,3 +7,9 @@ if (codingLanguages.length === 1) {
   const lastLanguage = codingLanguages.pop();
   footerMessage.textContent = `This page was built using: ${codingLanguages.join(", ")} and ${lastLanguage}.`;
 }
+
+const toggleSwitch = document.querySelector('.toggle-switch');
+
+toggleSwitch.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+});
