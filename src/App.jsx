55b326549f63ff2197ae1107952dcb8577b841d6
Xin <78652542+Him97@users.noreply.github.com>
2023-09-02 13:13:12 +0300

    M1 - synchronize with meta lab
    replace bootstrap with chakra;
    add formik, yup, router dom, fontawesome;
    update readme to lab instruction ver;

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  Box,
+  VStack,
+  Grid,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import Header from "./components/Header";
+import LandingSection from "./components/LandingSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactMeSection from "./components/ContactMeSection";
+import Footer from "./components/Footer";
+import { AlertProvider } from "./context/alertContext";
+import Alert from "./components/Alert";
+
+export default function App() {
+  return (
+    <AlertProvider>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Header />
+            <LandingSection />
+            <ProjectsSection />
+            <ContactMeSection />
+            <Footer />
+            <Alert />
+          </VStack>
+        </Grid>
+      </Box>
+    </AlertProvider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
 root.render(
-  <App />
+  <ChakraProvider theme={theme}>
+    <ColorModeScript />
+    <App />
+  </ChakraProvider>
 );
 
+serviceWorker.unregister();
 reportWebVitals();
